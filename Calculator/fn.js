bf799251c36ecb2403cc6cd914f2fa107e076d8f
SuyashShinde10 <157945381+SuyashShinde10@users.noreply.github.com>
2024-02-01 08:22:36 +0530

    Add files via upload

---
+++ Calculator/fn.js
@@ -0,0 +1,20 @@
+function add()
+{
+    const result =a+b;
+    window.alert(result);
+}
+function sub()
+{
+    const result =a-b;
+    window.alert(result);
+}
+function mul()
+{
+    const result =a*b;
+    window.alert(result);
+}
+function div()
+{
+    const result =a/b;
+    window.alert(result);
+}
---
+++ Calculator/pr.js
@@ -0,0 +1,6 @@
+const a=parseFloat(prompt("Enter no 1"));
+const b=parseFloat(prompt("Enter no 2"));
+document.write(a);
+document.write("<br>");
+document.write(b);
+document.write("<br>");
